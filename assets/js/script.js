90604370b446cf90983cf61f85d526f1ed3538a0
leandramusto@gmail.com <leandramusto@gmail.com>
2024-02-28 17:58:45 +0100

    add function to start the game,aadd reset to clean the grid,condition if it's a new game open grids,playcontent, add play again

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,27 @@
-console.log("hi there");
-//costante per il pulsante del game
-// add Event LISTENER TO BTN
-// FUNCTION to genarate the game
-
-//costante per il pulsante del game
-const myGamePadBtn = document.getElementById("game-pad-btn");
-// add Event LISTENER TO BTN
-myGamePadBtn.addEventListener("click", startGame);
-// FUNCTION to genarate the game
-function myGamePadBtnClick() {
+// startGame.js
+function startGame() {
   console.log("Start the game");
+
+  // Clear the grid of cells
+  reset();
+  console.log("Grid cleared");
+
+  // Get the difficulty level from the HTML select
+  var difficulty = parseInt(select.value);
+  console.log("Difficulty:", difficulty);
+
+  // Add a check - if it's a new game, open the grids
+  if (newGame) {
+    console.log("New game, open the grids");
+
+    // Based on the selected difficulty level, generate a grid of cells
+    switch (difficulty) {
+      // Easy: 10 cells per row
+
+
+    newGame = true;
+    myGamePadButton.textContent = "Play Again";
+  } else {
+    console.log("Same game, play again without changing the difficulty");
+  }
 }
-//funcciton reset to clean the griglia
-function reset();
-console.log("Griglia cleaned");
-CSSContainerRule.replaceElement();
