43c0fe4ac3fa117ea503fd552c25977bbfc4fdfb
leandramusto@gmail.com <leandramusto@gmail.com>
2024-02-28 23:56:12 +0100

    add do while cycle to fix the problem of reproducing more cells,almost done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,55 +1,20 @@
 // Get references to HTML elements
 const gamePadBtn = document.getElementById("game-pad-btn");
-const difficultySelect = document.getElementById("difficulty-select");
-const gridContainer = document.querySelector(".grid-container");
-const header = document.querySelector(".header");
+const gridContainer = document.getElementById("minesweeper-grid");
 
 // Add an event listener for the "Play" button
 gamePadBtn.addEventListener("click", generateMinesweeperGrid);
 
-// Function to generate Minesweeper grid based on difficulty
+// Function to generate Minesweeper grid
 function generateMinesweeperGrid() {
-  // Hide the difficulty level
-  difficultySelect.style.display = "none";
+  // Clear the content of the grid container
+  gridContainer.innerHTML = "";
 
-  // Get the selected difficulty level
-  const difficulty = parseInt(difficultySelect.value);
-
-  // Calculate the number of cells based on the difficulty
-  let totalCells;
-  let rows;
-  let columns;
-
-  switch (difficulty) {
-    case 1:
-      totalCells = 100;
-      rows = 10;
-      columns = 10;
-      break;
-    case 2:
-      totalCells = 81;
-      rows = 9;
-      columns = 9;
-      break;
-    case 3:
-      totalCells = 49;
-      rows = 7;
-      columns = 7;
-      break;
-    default:
-      totalCells = 100;
-      rows = 10;
-      columns = 10;
-      break;
-  }
-
-  let i = 1;
-
-  //  do-while loop
+  let cellNumber = 1;
   do {
     const cell = document.createElement("div");
-    cell.textContent = i;
-    cell.classList.add("SquareCells"); // Add the class for styling
+    cell.textContent = cellNumber;
+    cell.classList.add("SquareCells");
 
     // Add a click event listener for each cell
     cell.addEventListener("click", handleCellClick);
@@ -57,11 +22,8 @@ function generateMinesweeperGrid() {
     // Add the cell to the grid container
     gridContainer.appendChild(cell);
 
-    i++;
-  } while (i <= totalCells);
-
-  // Set the grid container styles based on the number of columns
-  gridContainer.style.gridTemplateColumns = `repeat(${columns}, 1fr)`;
+    cellNumber++;
+  } while (cellNumber <= 100);
 }
 
 // Function to handle the click event on each cell
